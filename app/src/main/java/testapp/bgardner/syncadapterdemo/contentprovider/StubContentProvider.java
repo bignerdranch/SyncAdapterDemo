b2f7c0b466b473ab63944540bd0b30e756f0f69c
Brian Gardner <brngardner@gmail.com>
2014-10-10 15:15:19 -0400

    Add stub content provider class

---
+++ app/src/main/java/testapp/bgardner/syncadapterdemo/contentprovider/StubContentProvider.java
@@ -0,0 +1,41 @@
+package testapp.bgardner.syncadapterdemo.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by bgardner on 10/10/14.
+ */
+public class StubContentProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return "";
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
