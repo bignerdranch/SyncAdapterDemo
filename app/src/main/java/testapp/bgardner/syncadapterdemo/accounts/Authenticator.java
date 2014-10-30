fc2141511f62784a57fdc2189d6a214f4c82392d
Brian Gardner <brngardner@gmail.com>
2014-10-30 12:41:11 -0400

    Fill out getAuthToken method in Authenticator
    * Check if we already have an auth token and return it if it is
    present
    * Return bundle configured with intent for AuthenticatorActivity
    if auth token is not present

--- app/src/main/java/testapp/bgardner/syncadapterdemo/accounts/Authenticator.java
+++ app/src/main/java/testapp/bgardner/syncadapterdemo/accounts/Authenticator.java
@@ -8,6 +8,7 @@ import android.accounts.NetworkErrorException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 
 import testapp.bgardner.syncadapterdemo.activities.AuthenticatorActivity;
 
@@ -43,7 +44,24 @@ public class Authenticator extends AbstractAccountAuthenticator {
 
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
-        return null;
+        final AccountManager accountManager = AccountManager.get(mContext);
+
+        String authToken = accountManager.peekAuthToken(account, authTokenType);
+
+        final Bundle result = new Bundle();
+        if (TextUtils.isEmpty(authToken)) {
+            final Intent intent = new Intent(mContext, AuthenticatorActivity.class);
+            intent.putExtra(AuthenticatorActivity.EXTRA_ACCOUNT_TYPE, account.type);
+            intent.putExtra(AuthenticatorActivity.EXTRA_AUTH_TYPE, authTokenType);
+            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+            result.putParcelable(AccountManager.KEY_INTENT, intent);
+            return result;
+        } else {
+            result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
+            result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
+            return result;
+        }
     }
 
     @Override
